180e2f449228c63789fd5f2a84b1e845bd771c67
Artur Keerd <arturkeerd@gmail.com>
2025-04-01 11:37:06 +0300

    Ülesanne 5

--- src/components/MealItem.js
+++ src/components/MealItem.js
@@ -1,13 +1,20 @@
 import { props } from "react";
 
 const MealItem = (props) => {
+    const formattedPrice = new Intl.NumberFormat(navigator.language, {
+        style: "currency",
+        currency: props.meal.currency || "EUR", // Default to EUR if no currency is provided
+    }).format(props.meal.price);
+
+    const formattedPriceWithSymbolAfter = formattedPrice.replace(/^(\D+)(\d.*)/, "$2 $1");
+
     return (
         <li>
             <article className="meal-item">
                 <img src={require(`../assets/${props.meal.image}`)} alt={props.meal.name}/>
                 <div>
                     <h3>{props.meal.name}</h3>
-                    <p className="meal-item-price">{props.meal.price}</p>
+                    <p className="meal-item-price">{formattedPriceWithSymbolAfter}</p>
                     <p className="meal-item-description">{props.meal.description}</p>
                 </div>
                 <p>
