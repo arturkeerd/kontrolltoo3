e0ed3baf4e2fa48f98c4e24e9584f01e75953631
Artur Keerd <arturkeerd@gmail.com>
2025-04-01 10:29:17 +0300

    Ülesanne 3

--- src/components/Meals.js
+++ src/components/Meals.js
@@ -1,4 +1,21 @@
+import { useState , useEffect } from "react"
+
 const Meals = () => {
+    const [meals, setMeals] = useState([])
+
+    useEffect(() => {
+        fetch("http://localhost:3001/meals")
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("Network response was not ok");
+                }
+                return response.json();
+            })
+            .then((data) => {
+                console.log(data)
+            })
+            .catch((error) => console.error("Error fetching meals:", error));
+    }, [])
     return (
         <ul id="meals">
             { 
